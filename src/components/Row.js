e5379622d94a16a91f9778819eb951b14ee9e563
suryatejaj97 <suryatejaj97@gmail.com>
2021-05-31 11:53:39 +0530

    added with trailer pop-up

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,12 +1,41 @@
 import React, { useState, useEffect } from "react";
+import YouTube from "react-youtube";
 import axios from "../axios";
+import movieTrailer from "movie-trailer"
 import "./Row.css"
 
-const baseImgUrl = "https://image.tmdb.org/t/p/original";
+const baseImgUrl = "https://image.tmdb.org/t/p/original/";
 
 // destructuring with props
 const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("")
+
+  // Options for react-youtube
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  }
+
+  const handleClick = async (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      let trailerurl = await axios.get(
+        `/movie/${movie.id}/videos?api_key=fef477e98a8c4c16a65b4ca7186eb891`
+      );
+      setTrailerUrl(trailerurl.data.results[0]?.key);
+      // movieTrailer(movie?.name || "")
+      // .then((url) => {
+      //   // https://www.youtube.com/watch?v=PPtVwZxdDc8 --> extracting 'v' from url
+      //   const urlParams = new URLSearchParams(new URL(url).search)
+      //   setTrailerUrl(urlParams.get("v"))
+      // })
+    }
+  }
 
   // Snipet of code which runs based on specific condition
   useEffect(() => {
@@ -20,7 +49,7 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
     // if [], run once when row loads, and dont run again
   }, [fetchUrl]);
 
-  console.log(movies);
+  // console.log(movies);
 
   return (
     <div className="row">
@@ -35,10 +64,11 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
             className={`row_poster ${isLargeRow && "row_posterLarge"}`}
             src={`${baseImgUrl}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
             alt={movie.name}
+            onClick={() => handleClick(movie)}
           />
         ))}
       </div>
-      {/* */}
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
